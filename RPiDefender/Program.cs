054a6c8158affc991ce64af6f4823fb3e7b6578b
Václav Janošek <vaclav.janosek@thermofisher.com>
2023-04-16 18:41:32 +0200

    deploy to Arm64

--- RPiDefender/Program.cs
+++ RPiDefender/Program.cs
@@ -14,6 +14,7 @@ namespace RPiDefender
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddSingleton<CameraService>();
 
             var app = builder.Build();
 
